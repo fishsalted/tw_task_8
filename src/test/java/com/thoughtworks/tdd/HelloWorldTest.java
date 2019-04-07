767fb8ee64a6f7d41c96d13557951b7ec92b5e98
fishsalted <10572677+fishsalted@users.noreply.github.com>
2019-04-07 20:10:29 +0800

    完成 fizzBuzz 函数测试

--- src/test/java/com/thoughtworks/tdd/HelloWorldTest.java
+++ src/test/java/com/thoughtworks/tdd/HelloWorldTest.java
@@ -1,54 +1,59 @@
 package com.thoughtworks.tdd;
 
-import com.thoughtworks.tdd.Dependency;
-import com.thoughtworks.tdd.HelloWorld;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 
 public class HelloWorldTest {
-
-    @BeforeAll
-    static void initTestEnv() {
-    }
-
-    @BeforeEach
-    void initEveryMethod() {
-    }
-
-
+    
     @Test
-    @DisplayName("try display name")
-    public void hello_world_test() {
-        //given
-        Dependency dependency = new Dependency();
-        HelloWorld helloWorld = new HelloWorld(dependency);
-
-        //when
-        String actual = helloWorld.beenCalled();
-
-        //then
-        assertThat(actual, is("Leave me alone."));
+    public void test_fizzBuzz_3() {
+        String result = HelloWorld.fizzBuzz(3);
+        
+        assertThat(result, is("Fizz"));
     }
 
     @Test
-    public void should_be_mocked() {
-        //given
-        Dependency dependency = mock(Dependency.class);
-        when(dependency.say()).thenReturn("Hello World");
-        HelloWorld helloWorld = new HelloWorld(dependency);
+    public void test_fizzBuzz_5() {
+        String result = HelloWorld.fizzBuzz(5);
 
-        //when
-        String actual = helloWorld.beenCalled();
+        assertThat(result, is("Buzz"));
+    }
 
-        //then
-        assertThat(actual, is("Hello World"));
+    @Test
+    public void test_fizzBuzz_7() {
+        String result = HelloWorld.fizzBuzz(7);
+
+        assertThat(result, is("Whizz"));
+    }
+
+    @Test
+    public void test_fizzBuzz_15() {
+        String result = HelloWorld.fizzBuzz(15);
+
+        assertThat(result, is("FizzBuzz"));
+    }
+
+    @Test
+    public void test_fizzBuzz_21() {
+        String result = HelloWorld.fizzBuzz(21);
+
+        assertThat(result, is("FizzWhizz"));
+    }
+
+    @Test
+    public void test_fizzBuzz_35() {
+        String result = HelloWorld.fizzBuzz(35);
+
+        assertThat(result, is("BuzzWhizz"));
+    }
+
+    @Test
+    public void test_fizzBuzz_105() {
+        String result = HelloWorld.fizzBuzz(105);
+
+        assertThat(result, is("FizzBuzzWhizz"));
     }
 }
