302860ddbea98746e43e8be93965c34fe47c03e2
fishsalted <10572677+fishsalted@users.noreply.github.com>
2019-04-07 20:05:42 +0800

    定义 fizzBuzz 函数

--- src/main/java/com/thoughtworks/tdd/HelloWorld.java
+++ src/main/java/com/thoughtworks/tdd/HelloWorld.java
@@ -4,13 +4,16 @@ package com.thoughtworks.tdd;
  * Created by twer on 3/28/14.
  */
 public class HelloWorld {
-    private Dependency dependency;
 
-    public HelloWorld(Dependency dependency) {
+    public static String fizzBuzz(int number) {
+        //todo
+    }
 
-        this.dependency = dependency;
+    public static void main(String[] args) {
+        for( int i = 0; i < 120; i++) {
+            String result = fizzBuzz(i+1);
+            System.out.println(result);
+        }
     }
-    public String beenCalled() {
-        return dependency.say();
-    }
+
 }
